f04ff3178c85596be4b0bf72a8cd4dbc6ed68dbc
catalamarti <marticatalasabate@gmail.com>
2025-11-05 02:08:31 +0000

    add analysis 101 to 119

--- R/buildAchilles.R
+++ R/buildAchilles.R
@@ -152,12 +152,21 @@ appendAchillesId <- function(cdm, id) {
   if (types[[1]] == "update") {
     res <- update(cdm = cdm)
   } else if (types[[1]] == "count") {
-    res <- counts(x = x, by = groupBy(analysis = analysis), count = types[[2]])
+    by <- groupBy(analysis = analysis)
+    res <- counts(x = x, by = by, count = types[[2]])
+  } else if (types[[1]] == "distribution") {
+    by <- groupBy(analysis = analysis)
+    res <- distribution(x = x, by = by, value = types[[2]])
   }
 
   # compute table
   nm <- omopgenerics::uniqueTableName()
-  res <- dplyr::compute(x = res, name = nm)
+  if (inherits(x = res, what = "data.frame")) {
+    cdm <- omopgenerics::insertTable(cdm = cdm, name = nm, table = res)
+    res <- cdm[[nm]]
+  } else {
+    res <- dplyr::compute(x = res, name = nm)
+  }
 
   # prepare res
   res <- prepareResult(res = res, id = id)
@@ -178,15 +187,137 @@ operation <- function(x, op) {
     return(x)
   }
 
-  op <- as.list(stringr::str_split_1(string = op, pattern = " "))
+  op <- stringr::str_split_1(string = op, pattern = ";") |>
+    as.list() |>
+    purrr::map(\(x) stringr::str_split_1(string = x, pattern = " "))
+
   cdm <- omopgenerics::cdmReference(table = x)
 
-  if (op[[1]] == "remove") {
-    table <- op[[2]]
-    col <- op[[3]]
-    x <- x |>
-      dplyr::filter(!is.na(.data[[col]])) |>
-      dplyr::anti_join(cdm[[table]], by = col)
+  for (k in seq_along(op)) {
+    act <- op[[k]]
+    if (act[1] == "remove") {
+      table <- act[2]
+      col <- act[3]
+      x <- x |>
+        dplyr::filter(!is.na(.data[[col]])) |>
+        dplyr::anti_join(cdm[[table]], by = col)
+    } else if (act[1] == "first") {
+      col <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      x <- x |>
+        dplyr::group_by(.data$person_id) |>
+        dplyr::filter(.data[[col]] == min(.data[[col]], na.rm = TRUE)) |>
+        dplyr::ungroup()
+    } else if (act[1] == "addAge") {
+      col <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      x <- x |>
+        PatientProfiles::addAgeQuery(indexDate = col)
+    } else if (act[1] == "addGenderConceptId") {
+      x <- x |>
+        dplyr::left_join(
+          cdm$person |>
+            dplyr::select("person_id", "gender_concept_id"),
+          by = "person_id"
+        )
+    } else if (act[1] == "addDuration") {
+      start <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      end <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "end_date")
+      x <- x |>
+        dplyr::mutate(duration = clock::date_count_between(
+          start = .data[[start]], end = .data[[end]], precision = "day"
+        ))
+    } else if (act[1] == "addAgeDecile") {
+      col <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      x <- x |>
+        PatientProfiles::addAgeQuery(indexDate = col) |>
+        dplyr::mutate(age_decile = floor(.data$age/10))
+    } else if (act[1] == "addDuration30d") {
+      col <- act[2]
+      start <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      end <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "end_date")
+      x <- x |>
+        dplyr::mutate(!!col := as.integer(floor(clock::date_count_between(
+          start = .data[[start]], end = .data[[end]], precision = "day"
+        )/30)))
+    } else if (act[1] == "addPeriod") {
+      start <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      end <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "end_date")
+      startYear <- x |>
+        dplyr::summarise(x = min(.data[[start]], na.rm = TRUE)) |>
+        dplyr::pull("x") |>
+        clock::get_year()
+      endYear <- x |>
+        dplyr::summarise(x = max(.data[[end]], na.rm = TRUE)) |>
+        dplyr::pull("x") |>
+        clock::get_year()
+      if (act[2] == "year") {
+        y <- dplyr::tibble(calendar_year = as.integer(startYear:endYear)) |>
+          dplyr::mutate(
+            start_date = as.Date(paste0(.data$calendar_year, "-01-01")),
+            end_date = as.Date(paste0(.data$calendar_year, "-12-31"))
+          )
+      } else if (act[2] == "month") {
+        y <- tidyr::expand_grid(year = startYear:endYear, month = 1:12) |>
+          dplyr::mutate(
+            calendar_month = as.integer(year * 100 + month),
+            start_date = as.Date(sprintf("%i-%02i-01", .data$year, .data$month)),
+            end_date = as.Date(clock::add_days(clock::add_months(.data$start_date, 1), -1))
+          ) |>
+          dplyr::select(!c("year", "month"))
+      }
+      nm <- omopgenerics::uniqueTableName()
+      cdm <- omopgenerics::insertTable(cdm = cdm, name = nm, table = y, temporary = TRUE)
+      x <- x |>
+        dplyr::cross_join(cdm[[nm]])
+      if (act[3] == "full") {
+        x <- x |>
+          dplyr::filter(
+            .data$start_date >= .data[[start]] & .data$end_date <= .data[[end]]
+          )
+      } else if (act[3] == "one") {
+        x <- x |>
+          dplyr::filter(
+            .data$start_date <= .data[[end]] & .data$end_date >= .data[[start]]
+          )
+      }
+    } else if (act[1] == "addDate") {
+      if (act[2] == "start") {
+        col <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      } else if (act[2] == "end") {
+        col <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "end_date")
+      }
+      if (act[3] == "year") {
+        x <- x |>
+          dplyr::mutate(calendar_year = as.integer(
+            clock::get_year(x = .data[[col]])
+          ))
+      } else if (act[3] == "month") {
+        x <- x |>
+          dplyr::mutate(calendar_month = as.integer(
+            clock::get_year(x = .data[[col]]) * 100 + clock::get_month(x = .data[[col]])
+          ))
+      }
+    } else if (act[1] == "addNumberObservationPeriods") {
+      x <- x |>
+        dplyr::left_join(
+          cdm$observation_period |>
+            dplyr::group_by(.data$person_id) |>
+            dplyr::tally(name = "number_of_observation_periods"),
+          by = "person_id"
+        ) |>
+        dplyr::mutate(number_of_observation_periods = dplyr::coalesce(
+          as.integer(.data$number_of_observation_periods), 0L
+        ))
+    } else if (act[1] == "removeRecordsAfterBirth") {
+      start <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      x <- x |>
+        PatientProfiles::addDateOfBirth(dateOfBirthName = "dob") |>
+        dplyr::filter(.data[[start]] < .data$dob)
+    } else if (act[1] == "removeEndAfterStart") {
+      start <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "start_date")
+      end <- omopgenerics::omopColumns(table = omopgenerics::tableName(x), field = "end_date")
+      x <- x |>
+        dplyr::filter(.data[[end]] < .data[[start]])
+    }
   }
   x
 }
@@ -208,7 +339,7 @@ groupBy <- function(analysis) {
   by <- analysis |>
     dplyr::select(dplyr::starts_with("stratum_")) |>
     as.list() |>
-    unlist() |>
+    purrr::map_chr(omopgenerics::toSnakeCase) |>
     purrr::keep(\(x) !is.na(x))
   names(by) <- stringr::str_remove(names(by), "_name$")
   return(by)
@@ -223,6 +354,23 @@ counts <- function(x, by, count) {
   x |>
     dplyr::group_by(dplyr::across(dplyr::all_of(by))) |>
     dplyr::summarise(!!!q)
+}
+distribution <- function(x, by, value) {
+  x |>
+    dplyr::select(dplyr::all_of(c(by, value = value))) |>
+    dplyr::collect() |>
+    dplyr::summarise(
+      count_value = as.integer(dplyr::n()),
+      min_value = as.integer(min(.data$value, na.rm = TRUE)),
+      max_value = as.integer(max(.data$value, na.rm = TRUE)),
+      avg_value = as.numeric(mean(.data$value, na.rm = TRUE)),
+      stdev_value = as.numeric(sd(.data$value, na.rm = TRUE)),
+      median_value = as.numeric(median(.data$value, na.rm = TRUE)),
+      p10_value = as.numeric(quantile(.data$value, probs = 0.10, na.rm = TRUE)),
+      p25_value = as.numeric(quantile(.data$value, probs = 0.25, na.rm = TRUE)),
+      p75_value = as.numeric(quantile(.data$value, probs = 0.75, na.rm = TRUE)),
+      p90_value = as.numeric(quantile(.data$value, probs = 0.90, na.rm = TRUE))
+    )
 }
 prepareResult <- function(res, id) {
   q <- paste0("stratum_", 1:5) |>

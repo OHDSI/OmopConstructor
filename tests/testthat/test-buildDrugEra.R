deefd04dc700c4b3dd3b1ad73e7800c19dc0daeb
Martí Català Sabaté <marticatalasabate@gmail.com>
2026-02-09 21:33:02 +0000

    fix notes in cran

--- tests/testthat/test-buildDrugEra.R
+++ tests/testthat/test-buildDrugEra.R
@@ -1,15 +1,62 @@
+
 test_that("test drug_era table", {
-  cdm <- omock::mockVocabularySet(vocabularySet = "empty_cdm_5.4") |>
-    omock::mockCdmFromTables(tables = list(
-      drug_exposure = dplyr::tibble(
-        person_id = c(1L, 1L, 1L, 1L, 2L, 2L),
-        drug_concept_id = c(41008582L, 41008583L, 41008585L, 41125807L, 40999501L, 40999501L),
-        drug_exposure_start_date = as.Date("2000-01-01") + c(0L, 150L, 300L, 170L, 20L, 180L),
-        drug_exposure_end_date = as.Date("2000-01-01") + c(200L, 250L, 350L, 270L, 165L, 200L),
-        verbatim_end_date = drug_exposure_end_date,
-        drug_exposure_id = 1:6L
-      )
-    )) |>
+
+  concept <- dplyr::tribble(
+    ~concept_id, ~concept_name, ~domain_id, ~vocabulary_id, ~concept_class_id,
+    ~standard_concept, ~concept_code, ~valid_start_date, ~valid_end_date,
+    ~invalid_reason,
+
+    # metformin
+    1503297L, "metformin", "Drug", "RxNorm", "Ingredient", "S", "6809",
+    as.Date("1970-01-01"), as.Date("2099-12-31"), NA,
+    40999501L, "Metformin 1000 MG Oral Tablet [Metformin Biomo] Box of 30",
+    "Drug", "RxNorm Extension", "Marketed Product", "S", "OMOP2197463",
+    as.Date("2017-08-24"), as.Date("2099-12-31"), NA,
+
+    # ibuprofen
+    1177480L, "ibuprofen", "Drug", "RxNorm", "Ingredient", "S", "5640",
+    as.Date("1970-01-01"), as.Date("2099-12-31"), NA,
+    41125807L, "Ibuprofen 200 MG Delayed Release Oral Tablet [Contraneural]",
+    "Drug", "RxNorm", "ingredient", "S", "OMOP2323769",
+    as.Date("1970-01-01"), as.Date("2099-12-31"), NA,
+
+    # acetaminophen
+    1125315L, "acetaminophen", "Drug", "RxNorm", "Ingredient", "S", "161",
+    as.Date("1970-01-01"), as.Date("2099-12-31"), NA,
+    41008582L, "Acetaminophen 125 MG Rectal Suppository [Paracetamol Bc] Box of 10 by Berlin-Chemie",
+    "Drug", "RxNorm Extension", "Marketed Product", "S", "OMOP2206544",
+    as.Date("2017-08-24"), as.Date("2099-12-31"), NA,
+    41008583L, "Acetaminophen 125 MG Rectal Suppository Box of 5 by MIP",
+    "Drug", "RxNorm Extension", "Marketed Product", "S", "OMOP2206545",
+    as.Date("2017-08-24"), as.Date("2099-12-31"), NA,
+    41008585L, "Acetaminophen 1000 MG Rectal Suppository Box of 10 by Azupharma",
+    "Drug", "RxNorm Extension", "Marketed Product", "S", "OMOP2206547",
+    as.Date("2017-08-24"), as.Date("2099-12-31"), NA,
+  )
+  concept_ancestor <- dplyr::tribble(
+    ~ancestor_concept_id, ~descendant_concept_id, ~min_levels_of_separation, ~max_levels_of_separation,
+    1125315L, 41008582L, 1L, 1L,
+    1125315L, 41008583L, 1L, 1L,
+    1125315L, 41008585L, 1L, 1L,
+    1177480L, 41125807L, 1L, 1L,
+    1503297L, 40999501L, 1L, 1L
+  )
+  drug_exposure <- dplyr::tibble(
+    person_id = c(1L, 1L, 1L, 1L, 2L, 2L),
+    drug_concept_id = c(41008582L, 41008583L, 41008585L, 41125807L, 40999501L, 40999501L),
+    drug_exposure_start_date = as.Date("2000-01-01") + c(0L, 150L, 300L, 170L, 20L, 180L),
+    drug_exposure_end_date = as.Date("2000-01-01") + c(200L, 250L, 350L, 270L, 165L, 200L),
+    verbatim_end_date = drug_exposure_end_date,
+    drug_exposure_id = 1:6L
+  )
+
+
+  cdm <- omock::mockCdmFromTables(tables = list(
+    concept = concept,
+    concept_ancestor = concept_ancestor,
+    drug_exposure = drug_exposure
+  )) |>
+    suppressWarnings() |>
     copyCdm()
 
   expect_no_error(cdm$drug_era <- buildDrugEra(cdm = cdm))

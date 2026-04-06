315c4c72d341a5dc2952718de5c14bc50050093f
catalamarti <marticatalasabate@gmail.com>
2026-04-06 22:22:35 +0100

    merge internal data

--- data-raw/internalData.R
+++ data-raw/internalData.R
@@ -1,18 +1,18 @@
 
 gest_est <- readr::read_csv(
-  file = here::here("extras", "pregnancyConcepts", "gest_est.csv"),
+  file = here::here("data-raw", "pregnancyConcepts", "gest_est.csv"),
   col_types = c(category = "c", gest_est_id = "i", preterm = "i", postterm = "i", fullterm = "i", nodata = "i")
 )
 outcome_limit <- readr::read_csv(
-  file = here::here("extras", "pregnancyConcepts", "outcome_limit.csv"),
+  file = here::here("data-raw", "pregnancyConcepts", "outcome_limit.csv"),
   col_types = c(first_preg_category = "c", outcome_preg_category = "c", outcome_limit_id = "i", min_days = "i")
 )
 pregnancy_concepts <- readr::read_csv(
-  file = here::here("extras", "pregnancyConcepts", "pregnancy_concepts.csv"),
+  file = here::here("data-raw", "pregnancyConcepts", "pregnancy_concepts.csv"),
   col_types = c(category = "c", data_value = "c", concept_id = "i", gest_value = "i")
 )
 term_durations <- readr::read_csv(
-  file = here::here("extras", "pregnancyConcepts", "term_durations.csv"),
+  file = here::here("data-raw", "pregnancyConcepts", "term_durations.csv"),
   col_types = c(category = "c", max_term = "i", min_term = "i", retry = "i")
 )
 
@@ -54,6 +54,7 @@ extraDetails <- readr::read_csv(
   file = here::here("data-raw", "achilles_analysis_extra_details.csv"),
   col_types = c(analysis_id = "i", type = "c", table = "c", operation = "c")
 )
+
 achillesAnalisisDetails <- achillesAnalisisDetails |>
   dplyr::left_join(extraDetails, by = "analysis_id")
 
